bb882b61fd78eb59484e57db90ee7e06b2dd9f1b
sulian.le_bozec_chiffoleau <sulian.le_bozec_chiffoleau@telecom-sudparis.eu>
2021-10-13 22:18:31 +0200

    Affichage Mes Réservations

--- agvoy-app/src/Controller/MesReservationsController.php
+++ agvoy-app/src/Controller/MesReservationsController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ReservationRepository;
+use App\Entity\Reservation;
 
 class MesReservationsController extends AbstractController
 {
     /**
      * @Route("/mes/reservations", name="mes_reservations")
      */
-    public function index(): Response
+    public function index(ReservationRepository $reservationRepository): Response
     {
+
         return $this->render('mes_reservations/index.html.twig', [
-            'controller_name' => 'MesReservationsController',
+            'reservations' => $reservationRepository->findAll(),
         ]);
     }
 }
